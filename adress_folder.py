ad7585c161cbb4e217c0310ae1e6fe02e4b3fc53
Alexandre Naaim <alexandre.naaim@univ-lyon1.fr>
2025-03-03 19:48:21 -0500

    add some file before transfer to CRME

---
+++ adress_folder.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+def path_global_c3d():
+    return Path("E:/Argos/Processing/C3D_labelled")
+
+def path_global_video():
+    return Path("E:/Argos/Processing/Formatted")
+
+def path_export_pose2d():
+    return Path("E:/Argos/Processing/Pose2d")
+
+def path_export_pose_framework():
+    return Path("E:/Argos/Processing/pose_framework")
+
