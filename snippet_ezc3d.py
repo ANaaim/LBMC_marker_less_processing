5471b5663aaf4521ed19cf3b4004d297b374e3af
Alexandre Naaim <alexandre.naaim@univ-lyon1.fr>
2025-08-06 14:05:41 -0500

    tranfering a function to add point to a c3d from a dictionary

--- snippet_ezc3d.py
+++ snippet_ezc3d.py
@@ -51,3 +51,37 @@ def add_point_from_dictionary(acq, point_to_add):
     acq["data"]["points"] = new_array
 
     return acq
+
+
+def create_c3d_from_dictionary(filename,point_to_add,fq,unit):
+    """Create a c3d file from a dictionary containing the points to add.
+    The dictionary should be in the following format:
+    point_to_add = {"point_name": np.array([[x1, y1, z1], [x2, y2, z2], ...])}
+    where x, y, z are the coordinates of the point in meters"""
+
+    # Create an empty c3d file
+    acq = ezc3d.c3d()
+    nb_frame = 0
+    for ind_point, (name_point, value_point) in enumerate(point_to_add.items()):
+        if value_point.shape[1] > nb_frame:
+            # If the point has more frames than the previous one, update the number of frames
+            nb_frame = value_point.shape[1]
+    nb_point = len(point_to_add)
+
+    new_array = np.zeros((4, nb_point, nb_frame))
+    new_array[3,:,:] = 1
+    new_list = []
+    # Add the points to the c3d file
+    for ind_point, (name_point, value_point) in enumerate(point_to_add.items()):
+        print(f"Adding point {name_point}")
+        nb_frame_temp = value_point.shape[1]
+        new_array[0:3, ind_point, :nb_frame_temp] = value_point
+        new_list.append(name_point)
+    
+    acq["data"]["points"] = new_array
+    acq["parameters"]["POINT"]["LABELS"]["value"] = new_list
+    acq['parameters']['POINT']['UNITS']['value'] = [unit]
+    acq['parameters']['POINT']['RATE']['value'] = [fq]
+    
+
+    acq.write(filename)
